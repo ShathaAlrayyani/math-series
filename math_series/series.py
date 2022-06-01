78a5e3d2713f328599ec0c18b34c5f9a07a723c0
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2022-06-01 12:11:41 +0300

    editing my function to become readable, then ran the tests and still passing the tests

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,8 @@
 def fibonacci(n):
-    if n>1:
-        return fibonacci(n-1)+fibonacci(n-2)
-    return n
+    if n <= 1:
+        return n
+    elif n > 1:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+
