b3da75515dc5d0bf862c3ce52c1beb5c587254bc
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2022-06-01 12:06:35 +0300

    putting the third test and i passed that test too without the need to edit my function

--- test/test_series.py
+++ test/test_series.py
@@ -1,7 +1,8 @@
 import pytest
 from math_series.series import fibonacci
 
-def test_one():
+
+def test_1():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
@@ -10,4 +11,10 @@ def test_one():
 def test_2():
     actual = fibonacci(3)
     expected = 2
+    assert actual == expected
+
+
+def test_3():
+    actual = fibonacci(7)
+    expected = 13
     assert actual == expected
